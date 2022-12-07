05c23a7682b0d5ee80793e0070e122d642a9704d
nemdong <ndw9797@naver.com>
2022-12-07 12:05:44 +0900

    employee, department, position - vo, xml

---
+++ src/main/java/com/community/vo/Department.java
@@ -0,0 +1,31 @@
+package com.community.vo;
+
+public class Department {
+
+	private int no;
+	private String name;
+	
+	public Department() {}
+	
+	public Department(int no) {
+		this.no = no;
+	}
+
+	public int getNo() {
+		return no;
+	}
+
+	public void setNo(int no) {
+		this.no = no;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+}
---
+++ src/main/java/com/community/vo/Employee.java
@@ -0,0 +1,94 @@
+package com.community.vo;
+
+import java.util.Date;
+
+public class Employee {
+	
+	private int empNo;
+	private String password;
+	private String name;
+	private String email;
+	private String phone;
+	private Department department;
+	private Position position;
+	private String type;
+	private String deleted;
+	private Date createdDate;
+	private Date updatedDate;
+	private Date deletedDate;
+	
+	public int getEmpNo() {
+		return empNo;
+	}
+	public void setEmpNo(int empNo) {
+		this.empNo = empNo;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getDeleted() {
+		return deleted;
+	}
+	public void setDeleted(String deleted) {
+		this.deleted = deleted;
+	}
+	public Date getCreatedDate() {
+		return createdDate;
+	}
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
+	}
+	public Date getUpdatedDate() {
+		return updatedDate;
+	}
+	public void setUpdatedDate(Date updatedDate) {
+		this.updatedDate = updatedDate;
+	}
+	public Date getDeletedDate() {
+		return deletedDate;
+	}
+	public void setDeletedDate(Date deletedDate) {
+		this.deletedDate = deletedDate;
+	}
+	public Department getDepartment() {
+		return department;
+	}
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	public Position getPosition() {
+		return position;
+	}
+	public void setPosition(Position position) {
+		this.position = position;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	
+}
+
