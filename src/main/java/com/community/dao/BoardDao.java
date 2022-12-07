471966817c6e39d27d1401c204cc0c6096483c20
nemdong <ndw9797@naver.com>
2022-12-08 00:35:37 +0900

    1207-nemdong-board

--- src/main/java/com/community/dao/BoardDao.java
+++ src/main/java/com/community/dao/BoardDao.java
@@ -1,5 +1,20 @@
 package com.community.dao;
 
+import java.util.List;
+
+import com.community.util.SqlMapper;
+import com.community.vo.Board;
+
 public class BoardDao {
 
+	private static BoardDao instance = new BoardDao();
+	private BoardDao() {}
+	public static BoardDao getInstance() {
+		return instance;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Board> getBoards() {
+		return (List<Board>) SqlMapper.selectList("boards.getBoards");
+	}
 }
