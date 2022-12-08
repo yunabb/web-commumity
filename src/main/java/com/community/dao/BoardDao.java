801440432ac45723e0bac1bae22b2dff48dbb51e
baeyuna <winelighttt@gmail.com>
2022-12-08 10:00:25 +0900

    board dao 입니다

--- src/main/java/com/community/dao/BoardDao.java
+++ src/main/java/com/community/dao/BoardDao.java
@@ -1,5 +1,29 @@
 package com.community.dao;
 
-public class BoardDao {
+import java.util.List;
 
+import com.community.util.SqlMapper;
+import com.community.vo.Board;
+
+public class BoardDao {
+	
+	public void insertBoard(Board board) {
+		SqlMapper.insert("boards.insertBoard", board);
+	}
+	
+	public List<Board> getBoards() {
+		return (List<Board>) SqlMapper.selectList("boards.getBoards");
+	}
+	
+	public List<Board> getBoardByNo(int boardNo) {
+		return (List<Board>) SqlMapper.selectList("boards.getBoardByNo", boardNo);
+	}
+	
+	public void updateBoard(Board board) {
+		SqlMapper.update("boards.updateBoard", board);
+	}
+	
+	public void deleteBoard(int boardNo) {
+		SqlMapper.delete("boards.deleteBoard", boardNo);
+	}
 }
